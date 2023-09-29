dbbb3eb7486230703f8c266ef523c40b059c73ff
DokterFaith <145459598+DokterFaith@users.noreply.github.com>
2023-09-29 09:52:13 +0200

    navbar en stylesheet toegevoegd

---
+++ header_f.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="navbar.css">
+
+    <title>Page Title</title>
+
+    <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <input type="text" placeholder="Search..">
+    </div>
+
+</head>
+
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+
+</html>
