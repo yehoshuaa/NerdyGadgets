8f22360047546ab2b332176ff9694c5cdc1e5e57
DokterFaith <145459598+DokterFaith@users.noreply.github.com>
2023-10-05 13:01:15 +0200

    nav styled

--- header_f.php
+++ header_f.php
@@ -1,16 +1,30 @@
 <!DOCTYPE html>
-<html>
+<html lang="Dutch">
+
 <head>
     <link rel="stylesheet" href="navbar.css">
-
     <title>Page Title</title>
 
-    <div class="topnav">
-        <a class="active" href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#contact">Contact</a>
-        <input type="text" placeholder="Search..">
+    <div class="bg-div">
+        <!-- Dit is de zoekbalk met een vergrootglas foto als submit button-->
+    <div class="search-container">
+        <img class="logo-img" src="logo.png" width="100" height="80" ALT="align box" >
+        <input type="text" placeholder="Zoeken...">
+        <input type="image" src= "search.png"
+               height="25px" width="25px"
+               alt="submit"/ >
     </div>
+
+<!-- Dit is de navbar met linkjes erin die nu leeg zijn -->
+
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
 
 </head>
 
---
+++ test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="Dutch">
+
+<head>
+    <link rel="stylesheet" href="navbar.css">
+    <title>Page Title</title><div class="search-container">
+        
+        <input type="text" placeholder="Zoeken...">
+        <input type="image" src= "search.png"
+               height="5px" width="5px"
+               alt="submit"/>
+
+
+    </div>
+
+<nav class="navigation-bar">
+    <img class="logo" src="logo.png">
+    <a href="#">Home</a>
+    <a href="#">Projects</a>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Get in Touch</a>
+</nav>
