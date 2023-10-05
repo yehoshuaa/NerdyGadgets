a0fe9c106839dc39514706755cbf9e29c01e3168
DokterFaith <145459598+DokterFaith@users.noreply.github.com>
2023-10-05 21:54:31 +0200

    searchbar

--- navbar.php
+++ navbar.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="test.css">
-    <title>Document</title>
+    <title>NerdyGadgets</title>
 </head>
 <body>
     <header>
@@ -14,13 +14,13 @@
 
         <div class="search-container">
             <input type="text" placeholder="Zoeken...">
-            <input type="image" src= "search.png" height="25px" width="25px" alt="submit" >
+            <input type="image" src= "search.png" height="15px" width="15px" alt="submit">
         </div>
         <nav class="navbar">
             <ul class="nav_links">
-                <li><a href="#">Services</a> </li>
-                <li><a href="#">Home</a> </li>
-                <li><a href="#">Services</a> </li>
+                <li><a href="#"></a>Home </li>
+                <li><a href="#"></a> Help</li>
+                <li><a href="#">Mijn account</a> </li>
 
 
             </ul>
