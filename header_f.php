060b175c9ca23b3c10deaf51f4a40dd14a530ef5
DokterFaith <145459598+DokterFaith@users.noreply.github.com>
2023-10-05 21:14:44 +0200

    Andere nav style

--- header_f.php
+++ header_f.php
@@ -7,13 +7,13 @@
 
     <div class="bg-div">
         <!-- Dit is de zoekbalk met een vergrootglas foto als submit button-->
-    <div class="search-container">
-        <img class="logo-img" src="logo.png" width="100" height="80" ALT="align box" >
-        <input type="text" placeholder="Zoeken...">
-        <input type="image" src= "search.png"
-               height="25px" width="25px"
-               alt="submit"/ >
-    </div>
+        <div class="search-container">
+            <img class="logo-img" src="logo.png" width="100" height="80" ALT="align box" >
+            <input type="text" placeholder="Zoeken...">
+            <input type="image" src= "search.png"
+                   height="25px" width="25px"
+                   alt="submit"/ >
+        </div>
 
 <!-- Dit is de navbar met linkjes erin die nu leeg zijn -->
 
--- test.php
+++ test.php
@@ -1,23 +1,40 @@
-<!DOCTYPE html>
-<html lang="Dutch">
-
+<!doctype html>
+<html lang="en">
 <head>
-    <link rel="stylesheet" href="navbar.css">
-    <title>Page Title</title><div class="search-container">
-        
-        <input type="text" placeholder="Zoeken...">
-        <input type="image" src= "search.png"
-               height="5px" width="5px"
-               alt="submit"/>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="test.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img class="logo"  src="logo.png" alt="logo">
+
+        <div class="search-container">
+            <input type="text" placeholder="Zoeken...">
+            <input type="image" src= "search.png" height="25px" width="25px" alt="submit" >
+        </div>
+        <nav class="navbar">
+            <ul class="nav_links">
+                <li><a href="#">Services</a> </li>
+                <li><a href="#">Home</a> </li>
+                <li><a href="#">Services</a> </li>
 
 
-    </div>
+            </ul>
 
-<nav class="navigation-bar">
-    <img class="logo" src="logo.png">
-    <a href="#">Home</a>
-    <a href="#">Projects</a>
-    <a href="#">About</a>
-    <a href="#">Services</a>
-    <a href="#">Get in Touch</a>
-</nav>
+        </nav>
+
+        <a class="cta" href="#"><button>Winkelwagen</button></a>
+
+
+
+
+    </header>
+
+
+
+</body>
+</html>
