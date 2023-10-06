c1b9c5bfdbff956d9782301efd9eb9e1b4df1bce
DokterFaith <145459598+DokterFaith@users.noreply.github.com>
2023-10-06 09:57:28 +0200

    icons

--- navbar.php
+++ navbar.php
@@ -2,39 +2,35 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="test.css">
+    <script src="https://kit.fontawesome.com/d8635543be.js" crossorigin="anonymous"></script>
+
     <title>NerdyGadgets</title>
 </head>
 <body>
     <header>
         <img class="logo"  src="logo.png" alt="logo">
 
-        <div class="search-container">
-            <input type="text" placeholder="Zoeken...">
-            <input type="image" src= "search.png" height="15px" width="15px" alt="submit">
+        <div class="wrap">
+            <div class="search">
+                <input type="text" class="searchTerm" placeholder="Zoeken...">
+                <button type="submit" class="searchButton"> </button>
+            </div>
         </div>
+
         <nav class="navbar">
             <ul class="nav_links">
-                <li><a href="#"></a>Home </li>
-                <li><a href="#"></a> Help</li>
-                <li><a href="#">Mijn account</a> </li>
-
-
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><i class="fa-regular fa-user"></i><a href="#">Account</a> </li>
             </ul>
-
         </nav>
 
-        <a class="cta" href="#"><button>Winkelwagen</button></a>
-
-
-
+        <a class="cta" href="#"><button><i class="fa-solid fa-cart-shopping"></i></button></a>
 
     </header>
-
-
 
 </body>
 </html>
