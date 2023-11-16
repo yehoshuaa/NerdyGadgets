18f87492d10c30d426468a6e8fa040e34f01bb90
DokterFaith <faith.hofwijks@gmail.com>
2023-11-16 20:52:39 +0100

    shoppingcart

--- shoppingCart.php
+++ shoppingCart.php
@@ -2,8 +2,10 @@
 <html>
 <head>
     <title>Shopping Cart UI</title>
-    <link rel="stylesheet" type="text/css" href="./style.css">
+    <link rel="stylesheet" type="text/css" href="css/shoppingCart.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Orbitron&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Orbitron&display=swap" rel="stylesheet">
 </head>
 <body>
 <div class="CartContainer">
@@ -14,20 +16,20 @@
 
     <div class="Cart-Items">
         <div class="image-box">
-            <img src="images/apple.png" style={{ height="120px" }} />
+            <img src="images/laptop.jpg" style={{ height="150px" }} />
         </div>
         <div class="about">
-            <h1 class="title">Apple Juice</h1>
-            <h3 class="subtitle">250ml</h3>
-            <img src="images/veg.png" style={{ height="30px" }}/>
+            <h1 class="title">HP laptop</h1>
+            <h3 class="subtitle">16 inch</h3>
+            <img src="images/laptop.jpg" style={{ height="30px" }}/>
         </div>
         <div class="counter">
-            <div class="btn">+</div>
-            <div class="count">2</div>
             <div class="btn">-</div>
+            <div class="count">1</div>
+            <div class="btn">+</div>
         </div>
         <div class="prices">
-            <div class="amount">$2.99</div>
+            <div class="amount">€999.99</div>
             <div class="save"><u>Save for later</u></div>
             <div class="remove"><u>Remove</u></div>
         </div>
@@ -35,20 +37,20 @@
 
     <div class="Cart-Items pad">
         <div class="image-box">
-            <img src="images/grapes.png" style={{ height="120px" }} />
+            <img src="images/laptop2.jpg" style={{ height="150px" }} />
         </div>
         <div class="about">
             <h1 class="title">Grapes Juice</h1>
-            <h3 class="subtitle">250ml</h3>
-            <img src="images/veg.png" style={{ height="30px" }}/>
+            <h3 class="subtitle">16 inch</h3>
+            <img src="images/laptop2.jpg" style={{ height="30px" }}/>
         </div>
         <div class="counter">
-            <div class="btn">+</div>
-            <div class="count">1</div>
             <div class="btn">-</div>
+            <div class="count">1</div>
+            <div class="btn">+</div>
         </div>
         <div class="prices">
-            <div class="amount">$3.19</div>
+            <div class="amount">€999.99</div>
             <div class="save"><u>Save for later</u></div>
             <div class="remove"><u>Remove</u></div>
         </div>
@@ -60,7 +62,7 @@
                 <div class="Subtotal">Sub-Total</div>
                 <div class="items">2 items</div>
             </div>
-            <div class="total-amount">$6.18</div>
+            <div class="total-amount">€999.99</div>
         </div>
         <button class="button">Checkout</button></div>
 </div>
