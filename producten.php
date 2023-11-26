17d3fe36dfa22728b67bb48066656ad2847b5355
yehoshuaa <145460057+yehoshuaa@users.noreply.github.com>
2023-11-26 22:30:37 +0100

    CSS IS BROKEN
    ????????????????????????

--- producten.php
+++ producten.php
@@ -2,7 +2,12 @@
 require_once 'connection.php';
 
 $sql = "Select * FROM product";
+$sqlProduct = "Select DISTINCT category FROM product";
+$sqlPrice = "Select price FROM product";
+
+$all_category= $conn->query($sqlProduct);
 $all_product = $conn->query($sql);
+$all_price = $conn ->query($sqlPrice);
 
 ?>
 <!DOCTYPE html>
@@ -20,70 +25,75 @@ $all_product = $conn->query($sql);
             <input type="image" src= "images/search.png" height="15px" width="15px" alt="submit">
         </div>
 
+        <button id="slideBtn" class="slide-btn">Slide Menu</button>
+        <div id="menu" class="slide-menu">
+    <?php 
+    while ($options = mysqli_fetch_assoc($all_category)) {
+        echo '<label><input type="checkbox" class="category-filter" name="category[]" value="' . $options['category'] . '"><span class="category-label">' . $options['category'] . '</span></label><br>';
+    }
+    ?>
+
+<button id="sort-highest-btn">Sort by Highest Price</button>
+<button id="sort-lowest-btn">Sort by Lowest Price</button>
+
+    </div>
+
         <nav class="navbar">
             <ul class="nav_links">
                 <li><a href="#"></a></li>
                 <li><a href="#"></a></li>
-                <li><i class="fa-regular fa-user"></i><a href="#"> Account</a> </li>
-            </ul>
+                <li><i class="fa-regular fa-user"></i><a href="#"> Account</a> </li>  
         </nav>
 
       <i style="background-color: white;padding: 10px 15px;border-radius: 10px"  class="fa-solid fa-cart-shopping"></i>
+
+      
     </header>
-    <nav class="menu-bar">
-      <button class="menu-btn" data-category="all">All</button>
-      <button class="menu-btn" data-category="Laptop">Laptops</button>
-      <button class="menu-btn" data-category="PC">PCs</button>
-      <button class="menu-btn" data-category="display">Accessories</button>
-    </nav>
 
-  <?php
-while($row = mysqli_fetch_assoc($all_product)){
-?>
+
+    <?php
+while ($row = mysqli_fetch_assoc($all_product)) {
+    ?>
     <!-- header aka menubar -->
-    
+    <!-- producten -->
+    <section class="ProductSection">
+        <!-- product box -->
+        <div class="productbox <?php echo $row['category']; ?>">
+            <!-- Product Image -->
+            <div class="p-img-container">
+                <div class="p-img">
+                    <a href="#">
+                        <img src="http://via.placeholder.com/220x220" alt="Front">
+                    </a>
+                </div>
+            </div>
 
-   <!-- producten -->
-      <section class="ProductSection">
+            <!-- product text -->
+            <div class="p-box-text">
 
- <!-- product box 1 -->
-<div class="productbox">
-
-<!-- Product Image -->
-<div class="p-img-container">
-  <div class="p-img">
-    <a href="#">
-      <img src="http://via.placeholder.com/220x220" alt="Front">
-    </a>
-  </div>
-</div>
-
-<!-- product text -->
-<div class="p-box-text">
-
-<!-- product category -->
-  <div class="product-category">
-    <span><?php echo $row["category"]?></span>
-  </div>
-  <!-- Title -->
-  <a href="#" class="product-title">
-   <?php echo $row["name"]?>
-  </a>
-  <!-- price -->
-  <div class="price-buy">
-    <span class="p-price"><?php echo $row["price"]?></span>
-    <a href="#" class="p-buy-btn">Buy Now</a>
-  </div>
-    <!-- Description -->
-    <div class="description">
-    <?php echo $row["description"]?>
-    </div>
- </div>
-</div>
-<?php
-};
+                <!-- product category -->
+                <div class="product-category">
+                    <span><?php echo $row["category"] ?></span>
+                </div>
+                <!-- Title -->
+                <a href="#" class="product-title">
+                <?php echo strlen($row["name"]) > 22 ? substr($row["name"], 0, 22) . '...' : $row["name"]; ?>
+                </a>
+                <!-- price -->
+                <div class="price-buy">
+                    <span class="p-price"><?php echo $row["price"] ?></span>
+                    <a href="#" class="p-buy-btn">Buy Now</a>
+                </div>
+                <!-- Description -->
+                <div class="description">
+                <?php echo strlen($row["description"]) > 300 ? substr($row["description"], 0, 300) . '...' : $row["description"]; ?>
+                </div>
+            </div>
+        </div>
+    <?php
+}
 ?>
-      </section>
+</section>
 
 <!-- footer -->
 <div class="footer">
@@ -121,61 +131,128 @@ while($row = mysqli_fetch_assoc($all_product)){
       </div>
       <div class="test">
           <p align="right"> © Nerdy Gadgets 2023</p>
-
-
-
       </div>
 </body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 <script>
-  // Search function for all the products
+// filter op category
+$(document).ready(function() {
+        // Function to filter products based on selected categories
+        function filterProducts() {
+            var selectedCategories = [];
+            // Get all checked checkboxes
+            $('.category-filter:checked').each(function() {
+                selectedCategories.push($(this).val());
+            });
 
-  // Get all product boxes
-const productBoxes = document.querySelectorAll('.productbox');
+            // Show all products if no categories are selected
+            if (selectedCategories.length === 0) {
+                $('.productbox').show();
+            } else {
+                // Hide all products initially
+                $('.productbox').hide();
 
-// Function to handle the search
-function handleSearch() {
-  const searchValue = document.getElementById('searchInput').value.toLowerCase();
+                // Show products that belong to selected categories
+                selectedCategories.forEach(function(category) {
+                    $('.' + category).show();
+                });
+            }
+        }
 
-  productBoxes.forEach((box) => {
-    const title = box.querySelector('.product-title').textContent.toLowerCase();
-    if (title.includes(searchValue)) {
-      box.style.display = 'block'; // Show the matching product
-    } else {
-      box.style.display = 'none'; // Hide non-matching products
+        // Call filter function when checkboxes change
+        $('.category-filter').change(filterProducts);
+    });
+
+
+
+        document.addEventListener('DOMContentLoaded', function() {
+            var slideBtn = document.getElementById('slideBtn');
+            var menu = document.getElementById('menu');
+
+            slideBtn.addEventListener('click', function() {
+                // Toggle the menu visibility on button click
+                var menuStyle = window.getComputedStyle(menu);
+                var menuLeft = menuStyle.getPropertyValue('left');
+
+                if (menuLeft === '-400px') {
+                    menu.style.left = '0';
+                } else {
+                    menu.style.left = '-400px';
+                }
+            });
+        });
+
+// highest lowest sort functie
+        $(document).ready(function() {
+    var productsContainer = $('.ProductSection');
+    var products = $('.productbox').get();
+
+    // Function to sort products by price in ascending order
+    function sortByLowestPrice() {
+        products.sort(function(a, b) {
+            var priceA = parseFloat($(a).find('.p-price').text());
+            var priceB = parseFloat($(b).find('.p-price').text());
+            return priceA - priceB;
+        });
+
+        // Re-append sorted products to the container
+        productsContainer.empty();
+        $(products).each(function() {
+            productsContainer.append(this);
+        });
     }
-  });
-}
-// Event listener for the input event (typing in the search bar)
-document.getElementById('searchInput').addEventListener('input', handleSearch);
 
+    // Function to sort products by price in descending order
+    function sortByHighestPrice() {
+        products.sort(function(a, b) {
+            var priceA = parseFloat($(a).find('.p-price').text());
+            var priceB = parseFloat($(b).find('.p-price').text());
+            return priceB - priceA;
+        });
 
-// Filter function by category
-
-// Function to handle filtering based on the selected category
-function filterProducts(category) {
-  const productBoxes = document.querySelectorAll('.productbox');
-  
-  productBoxes.forEach((box) => {
-    const productCategory = box.querySelector('.product-category span').textContent;
-
-    if (category === 'all' || productCategory === category) {
-      box.style.display = 'block'; // Show the matching product
-    } else {
-      box.style.display = 'none'; // Hide non-matching products
+        // Re-append sorted products to the container
+        productsContainer.empty();
+        $(products).each(function() {
+            productsContainer.append(this);
+        });
     }
-  });
-}
 
-// Event listener for menu button clicks
-document.querySelectorAll('.menu-btn').forEach((button) => {
-  button.addEventListener('click', function () {
-    const selectedCategory = this.getAttribute('data-category');
-    filterProducts(selectedCategory);
-  });
+    // Event listeners for sorting buttons
+    $('#sort-highest-btn').click(function() {
+        sortByHighestPrice();
+    });
+
+    $('#sort-lowest-btn').click(function() {
+        sortByLowestPrice();
+    });
 });
+        
 
-// Initially display all products
-filterProducts('all');
+// searchbar functie
+$(document).ready(function() {
+    // Function to filter products based on search input
+    function filterSearch() {
+        var searchTerm = $('#searchInput').val().toLowerCase();
+        var products = $('.productbox');
+
+        products.each(function() {
+            var productName = $(this).find('.product-title').text().toLowerCase();
+            if (productName.includes(searchTerm)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    }
+
+    // Call filterSearch function when search input changes
+    $('#searchInput').on('input', filterSearch);
+    
+    // ... Rest of your existing JavaScript code ...
+
+});
 
 </script>
 </html>
