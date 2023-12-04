f0c87eaefd704e4415bbd700bbeab7cb21951adb
yehoshuaa <jozuab@hotmail.com>
2023-12-04 17:21:52 +0100

    menubar

--- producten.php
+++ producten.php
@@ -3,6 +3,7 @@
 <head>
 <link rel="stylesheet" href="producten.css">
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script src="https://cdn.tailwindcss.com/3.3.5"></script>
 </head>
 <!-- Makes the connection to the database and pulls the data from it -->
 <?php
@@ -22,15 +23,59 @@ $all_image = $conn ->query($sqlImage);
 
 <header>
 
-        <i style="background-color: white;padding: 10px 15px;border-radius: 10px"  class="fa-solid fa-cart-shopping"></i>
-        <img style="background-color: white; border-radius: 100px" class="logo" src="images/logo.png" alt="logo">
-        <div class="search-container">
-            <input id="searchInput" style="height: 40px;width: 20rem" type="text" class="input" placeholder="Zoeken...">
-            <input type="image" src= "images/search.png" height="15px" width="15px" alt="submit">
+<nav class="bg-[#36454f] border-gray-200 px-4 lg:px-6 py-2.5">
+        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+            <a href="/index.php" class="flex items-center">
+                <img src="../images/nerdygadgetslogo.png" width="229px" height="166.05" alt="NerdyGadgets logo" />
+            </a>
+            <div class="flex items-center lg:order-2">
+                <?php if (!isset($_SESSION['id'])){?>
+                    <a href="PHP/login_handling/login.php" class="text-gray-800 text-white hover:bg-gray-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Log in</a>
+                <?php }else{?>
+                    <i class="cursor-pointer py-3 px-3.5 mr-3 rounded-lg text-white fa-regular fa-user hover:text-black hover:bg-white"></i>
+                <?php }?>
+                <i class="cursor-pointer py-3 px-3 bg-white rounded-lg fa-solid fa-cart-shopping hover:bg-gray-600 hover:text-white"></i>
+
+                <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-2" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                    <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+            </div>
+            <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                    <li>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0" aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Company</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Marketplace</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Features</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Team</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Contact</a>
+                    </li>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </div>
+                        <input type="search" id="default-search" class="h-5 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
+                    </div>
+                </ul>
+            </div>
         </div>
-        <button id="slideBtn" class="slide-btn">Slide Menu</button>
+    </nav>
 
-
+    <button id="slideBtn" class="slide-btn">Slide Menu</button>
         <div id="menu" class="slide-menu">
          <?php 
             while ($options = mysqli_fetch_assoc($all_category)) {
@@ -46,25 +91,16 @@ $all_image = $conn ->query($sqlImage);
              Max Price: <span id="maxPrice"></span>
             </p>
         </div>
-            <nav class="navbar">
-            <ul class="nav_links">
-            <li><a href="#"></a></li>
-            <li><a href="#"></a></li>
-            <li><i class="fa-regular fa-user"></i><a href="#"> Account</a> </li>  
-            </nav>
+
+       
+ 
         </header>
 
 
     
     <section class="ProductSection">
     <?php
-    $count = 0;
-    echo '<div class="productRow">'; 
     while ($row = mysqli_fetch_assoc($all_product)) {
-        if ($count > 0 && $count % 4 == 0) {
-            echo '</div>'; 
-            echo '<div class="productRow">'; 
-        }
         ?>
         <!-- product box -->
         <div class="productbox <?php echo $row['category']; ?>">
@@ -98,11 +134,7 @@ $all_image = $conn ->query($sqlImage);
                 </div>
             </div>
         </div>
-        <?php
-        $count++;
-    }
-    echo '</div>'; 
-    ?>
+        <?php } ?>
 </section>
 
 <!-- footer -->
@@ -148,7 +180,7 @@ $all_image = $conn ->query($sqlImage);
 <script>
 // filter op category
 $(document).ready(function() {
-        // Function to filter products based on selected categories
+        //filter products based on selected categories
         function filterProducts() {
             var selectedCategories = [];
             // Get all checked checkboxes
@@ -192,6 +224,7 @@ $(document).ready(function() {
                 }
             });
         });
+
 
 // highest lowest sort functie
         $(document).ready(function() {
